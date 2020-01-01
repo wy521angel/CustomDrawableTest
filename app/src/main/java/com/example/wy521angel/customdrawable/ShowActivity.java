027fe64d773b1f9b50c030c94dc78732d8f9e03b
DESKTOP-NO9V8JL\dell <693962802@qq.com>
2020-01-01 23:40:25 +0800

    通过ClipDrawable裁剪实现图片徐徐展开

--- app/src/main/java/com/example/wy521angel/customdrawable/ShowActivity.java
+++ app/src/main/java/com/example/wy521angel/customdrawable/ShowActivity.java
@@ -2,6 +2,7 @@ package com.example.wy521angel.customdrawable;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,12 +11,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ShowActivity extends AppCompatActivity {
 
-    private static ImageView imageView;
+    private ImageView imageView;
+
+    private final static int IS_CONTUNUE = 0x22;
+    private ClipDrawable clipDrawable;
+    private ImageView imageView2;
+    Timer timer = new Timer();
 
     @SuppressLint("HandlerLeak")
-    private static Handler handler = new Handler() {
+    private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             if (msg.what == 1) {
@@ -24,6 +33,12 @@ public class ShowActivity extends AppCompatActivity {
                 imageView.getDrawable().setLevel(2);
             } else if (msg.what == 3) {
                 imageView.getDrawable().setLevel(3);
+            } else if (msg.what == IS_CONTUNUE) {
+                /**setlevel()设置图片截取的大小
+                 * 修改ClipDrawable的level值，level值为0--10000；
+                 * 10000：截取图片大小为空白，0：截取图片为整张图片；
+                 */
+                clipDrawable.setLevel(clipDrawable.getLevel() + 200);
             }
         }
     };
@@ -50,6 +65,23 @@ public class ShowActivity extends AppCompatActivity {
                 View view = findViewById(R.id.gemIv2);
                 TransitionDrawable drawable = (TransitionDrawable) view.getBackground();
                 drawable.startTransition(4000);
+
+                //通过裁剪实现图片徐徐展开
+                imageView2 = (ImageView) findViewById(R.id.gemIv3);
+                //获取图片所显示的ClipDrawable对象
+                clipDrawable = (ClipDrawable) imageView2.getDrawable();
+                //定时器
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        Message msg = new Message();
+                        msg.what = IS_CONTUNUE;
+                        handler.sendMessage(msg);
+                        if (clipDrawable.getLevel() >= 10000) {
+                            timer.cancel();
+                        }
+                    }
+                }, 0, 200);
                 break;
             default:
         }
